3fd80ce15fff8e294b79d1db9737c45791164a64
Varun Gupta <varun.gupta@airtel.com>
2023-07-08 09:41:52 +0530

    removed shimmer effect

--- app/src/main/java/com/gojek/demo/ui/RepositoryFragment.kt
+++ app/src/main/java/com/gojek/demo/ui/RepositoryFragment.kt
@@ -1,31 +1,30 @@
 package com.gojek.demo.ui
 
+import android.app.ProgressDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
-import android.widget.LinearLayout
+import android.widget.ProgressBar
 import android.widget.RelativeLayout
 import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.facebook.shimmer.ShimmerFrameLayout
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import com.gojek.demo.R
 import com.gojek.demo.ui.adapter.RepoListAdapter
 import com.gojek.demo.ui.viewmodel.BaseViewModel
 import com.gojek.demo.ui.viewmodel.RepoViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.util.Objects
 import javax.inject.Inject
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,32 +58,11 @@ class RepositoryFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_repository, container, false)
     }
 
-  /*  companion object {
-        *//**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RepositoryFragment.
-         *//*
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RepositoryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }*/
-
   val mViewModel: RepoViewModel by activityViewModels()
 
     @Inject
     lateinit var mRepoAdapter: RepoListAdapter
 
-    lateinit var mShimmerLayout: ShimmerFrameLayout
     lateinit var mRepoRecyclerview: RecyclerView
     lateinit var mErrorContainer: RelativeLayout
     lateinit var mRetryButton: AppCompatButton
@@ -103,10 +81,12 @@ class RepositoryFragment : Fragment() {
 
     //Initialize the views
     fun initViews(view: View) {
-        mShimmerLayout = view.findViewById(R.id.shimmer_layout)
         mRepoRecyclerview = view.findViewById(R.id.repo_recyclerview)
         mErrorContainer = view.findViewById(R.id.error_container)
         mRetryButton = view.findViewById(R.id.retry_button)
+        val progressBar = ProgressDialog(requireActivity())
+        progressBar.setMessage("Please Wait....")
+        this@RepositoryFragment.progressBar = progressBar
     }
 
     //set the list adapter
@@ -123,42 +103,48 @@ class RepositoryFragment : Fragment() {
             mViewModel.observeViewStateLiveData().observe(viewLifecycleOwner, { it ->
                 when (it) {
                     BaseViewModel.ViewStateType.SUCCESS -> {
-                        hideLoading(true)
+                        onHideLoading(true)
                     }
                     BaseViewModel.ViewStateType.ERROR -> {
-                        hideLoading(false)
+                        onHideLoading(false)
                     }
                     BaseViewModel.ViewStateType.LOADING -> {
-                        showLoading()
+                        onLoading()
                     }
                 }
             })
         }
     }
 
-    //stop the shimmer animation
-    fun stopShimmerEffect() {
-        mShimmerLayout.stopShimmerAnimation()
-        mShimmerLayout.visibility = GONE
-    }
-
-    //show the shimmer animation
-    fun showShimmerEffect() {
-        mShimmerLayout.visibility = VISIBLE
-        mShimmerLayout.startShimmerAnimation()
-    }
 
     //show the loader
-    fun showLoading() {
-        showShimmerEffect()
+    fun onLoading() {
+        showLoadingProgress()
         mRepoRecyclerview.visibility = GONE
         mErrorContainer.visibility = GONE
     }
 
+    lateinit var progressBar:ProgressDialog
+    //show the loader
+    fun showLoadingProgress() {
+
+
+    //    if(Objects.isNull(progressBar)) {
+
+     //   }
+        progressBar?.show()
+    }
 
     //hide the loader
-    fun hideLoading(isSucces: Boolean) {
-        stopShimmerEffect()
+    fun hideLoadingProgress() {
+        progressBar?.dismiss()
+
+    }
+
+
+    //hide the loader
+    fun onHideLoading(isSucces: Boolean) {
+        hideLoadingProgress()
         if (isSucces) {
             mErrorContainer.visibility = GONE
             mRepoRecyclerview.visibility = VISIBLE
